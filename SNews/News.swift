c69ad8d51d849df742a5cd6a835a54dfc5a1f38c
Sukhrat <sukhrat.arziyev@gmail.com>
2017-02-07 00:47:24 +0600

    Designed main storyboard and added IBOutlets

---
+++ SNews/News.swift
@@ -0,0 +1,19 @@
+//
+//  News.swift
+//  SNews
+//
+//  Created by Sukhrat on 07.02.17.
+//  Copyright © 2017 Sukhrat. All rights reserved.
+//
+
+import Foundation
+
+class News {
+    
+    private var _author: String!
+    private var _text: String!
+    private var _title: String!
+    
+    
+    
+}
---
+++ SNews/NewsCell.swift
@@ -0,0 +1,17 @@
+//
+//  NewsCell.swift
+//  SNews
+//
+//  Created by Sukhrat on 07.02.17.
+//  Copyright © 2017 Sukhrat. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var newsTitle: UILabel!
+    @IBOutlet weak var newsAuthor: UILabel!
+
+}
